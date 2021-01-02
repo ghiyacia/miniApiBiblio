c81eb7d1fa9fbf61f0421319a05adc2031768f1d
ghiyacia <ghilesyacia@gmail.com>
2021-01-02 03:47:54 +0100

    Creat Entity and config Bundle esayAdmin

---
+++ src/Controller/ApiGenreController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\GenreRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ApiGenreController extends AbstractController
+{
+    /**
+     * @Route("/api/genres", name="api_genres", methods={"GET"})
+     */
+    public function list(GenreRepository $repo, SerializerInterface $serializer): Response
+    {
+        return $this->render('api_genre/index.html.twig', [
+            'controller_name' => 'ApiGenreController',
+        ]);
+    }
+}
